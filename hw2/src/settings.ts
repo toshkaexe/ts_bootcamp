fb6176584466cc5133865b889fc4f3b294c91bd1
Anton <anton.zeltser@gmail.com>
2023-12-17 17:40:01 +0100

    test /env

--- hw2/src/settings.ts
+++ hw2/src/settings.ts
@@ -6,7 +6,12 @@ import {testingRoute} from "./routes/testing-route";
 export const app: Express = express();
 app.use(express.json())
 
-
+app.get('/env', (req, res) => {
+    res.send({
+        login: process.env.AUTH_LOGIN,
+        pass: process.env.AUTH_PASSWORD,
+    })
+})
 app.use('/blogs', blogRoute)
 app.use('/posts', postRoute)
 app.use('/testing', testingRoute)
